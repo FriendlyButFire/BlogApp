84eebf88cb650e69e6f7860dbc38a200bc11bf0e
xXAnToS12Xx <antoguada96@gmail.com>
2021-05-06 13:24:49 +0200

    Aggiunta bozza per dettagli dei post.

---
+++ app/src/main/java/it/edu/iisfalcone_righi/blog/Activities/PostDetailActivity.java
@@ -0,0 +1,18 @@
+package it.edu.iisfalcone_righi.blog.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import it.edu.iisfalcone_righi.blog.R;
+
+public class PostDetailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_detail);
+    }
+}
